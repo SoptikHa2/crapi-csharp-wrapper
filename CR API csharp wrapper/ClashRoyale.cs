508badf4d3ecad88ff6d3b252f14c63d67b10b42
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2017-11-29 22:35:06 +0100

    Minor changes to base wrapper class
    Changed Version to ApiVersion
    Adde WrapperVersion
    Changed class description

--- CR API csharp wrapper/ClashRoyale.cs
+++ CR API csharp wrapper/ClashRoyale.cs
@@ -1,22 +1,31 @@
 ﻿using System;
-using System.IO;
-using System.Net;
 
 namespace CRAPI
 {
     /// <summary>
-    /// Base CR API class, contains basic information
+    /// Base CR API class, contains basic information, like API version, or wrapper version. Can be used to get top players and top clans.
     /// </summary>
     public static class ClashRoyale
     {
+        private const string wVersion = "0.1.0";
+
         /// <summary>
         /// Get current API version in format like "4.0.3"
         /// </summary>
-        public static string Version
+        public static string ApiVersion
         {
             get
             {
                 return Inside.APIworker.Get(Inside.APIworker.Endpoints.Version, "");
+            }
+            set { }
+        }
+
+        public static string WrapperVersion
+        {
+            get
+            {
+                return wVersion;
             }
             set { }
         }
