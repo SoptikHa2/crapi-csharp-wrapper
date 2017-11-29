1e211af1dc720e0bc41995fb384539427f4d1a14
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2017-11-29 19:52:20 +0100

    Moved inside logic inside Inside.APIworker

---
+++ CR API csharp wrapper/APIworker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CRAPI.Inside
+{
+    public class APIworker
+    {
+        private const string domain = "http://api.cr-api.com/";
+
+
+        /// <summary>
+        /// All current API endpoints
+        /// </summary>
+        public enum Endpoints
+        {
+            Profile,
+            Top,
+            Clan,
+            Constants,
+            Version
+        }
+
+
+        /// <summary>
+        /// Get direct output from CR API
+        /// </summary>
+        /// <param name="endpoint">Endpoint to use</param>
+        /// <param name="parameter">Parameter to endpoint (like player ID)</param>
+        /// <returns></returns>
+        public static string Get(Endpoints endpoint, string parameter)
+        {
+            return Get(domain + endpoint.ToString() + "/" + parameter);
+        }
+
+        private static string Get(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            WebResponse myResponse = req.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            return result;
+        }
+    }
+}
--- CR API csharp wrapper/ClashRoyale.cs
+++ CR API csharp wrapper/ClashRoyale.cs
@@ -9,21 +9,6 @@ namespace CRAPI
     /// </summary>
     public static class ClashRoyale
     {
-        private const string domain = "http://api.cr-api.com/";
-        
-
-        /// <summary>
-        /// All current API endpoints
-        /// </summary>
-        public enum Endpoints
-        {
-            Profile,
-            Top,
-            Clan,
-            Constants,
-            Version
-        }
-
         /// <summary>
         /// Get current API version in format like "4.0.3"
         /// </summary>
@@ -31,33 +16,11 @@ namespace CRAPI
         {
             get
             {
-                return Get(Endpoints.Version, "");
+                return Inside.APIworker.Get(Inside.APIworker.Endpoints.Version, "");
             }
             set { }
         }
 
-        /// <summary>
-        /// Get direct output from CR API
-        /// </summary>
-        /// <param name="endpoint">Endpoint to use</param>
-        /// <param name="parameter">Parameter to endpoint (like player ID)</param>
-        /// <returns></returns>
-        public static string Get(Endpoints endpoint, string parameter)
-        {
-            return Get(domain + endpoint.ToString() + "/" + parameter);
-        }
-
-        private static string Get(string url)
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            req.Method = "GET";
-            WebResponse myResponse = req.GetResponse();
-            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
-            string result = sr.ReadToEnd();
-            sr.Close();
-            myResponse.Close();
-
-            return result;
-        }
+        
     }
 }
