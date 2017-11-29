7280cd8c5f86672c056f93c05b432fd932e85310
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2017-11-29 19:47:20 +0100

    Added base ClashRoyale class and debug class
    ClashRoyale class contains all basic functionality - downloads source from URL, can be used to get API version.

---
+++ CR API csharp wrapper/ClashRoyale.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CRAPI
+{
+    /// <summary>
+    /// Base CR API class, contains basic information and net methods
+    /// </summary>
+    public static class ClashRoyale
+    {
+        private const string domain = "http://api.cr-api.com/";
+        
+
+        /// <summary>
+        /// All current API endpoints
+        /// </summary>
+        public enum Endpoints
+        {
+            Profile,
+            Top,
+            Clan,
+            Constants,
+            Version
+        }
+
+        /// <summary>
+        /// Get current API version in format like "4.0.3"
+        /// </summary>
+        public static string Version
+        {
+            get
+            {
+                return Get(Endpoints.Version, "");
+            }
+            set { }
+        }
+
+        /// <summary>
+        /// Get direct output from CR API
+        /// </summary>
+        /// <param name="endpoint">Endpoint to use</param>
+        /// <param name="parameter">Parameter to endpoint (like player ID)</param>
+        /// <returns></returns>
+        public static string Get(Endpoints endpoint, string parameter)
+        {
+            return Get(domain + endpoint.ToString() + "/" + parameter);
+        }
+
+        private static string Get(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            WebResponse myResponse = req.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            return result;
+        }
+    }
+}
