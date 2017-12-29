4235b754ae14fe22ad29129f607da2134b90fba2
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2017-12-29 16:13:02 +0100

    Added more examples

--- ExampleUsage/Program.cs
+++ ExampleUsage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using CRAPI;
 
 namespace ExampleUsage
@@ -7,10 +8,16 @@ namespace ExampleUsage
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executing async requests. Press any key to end async testing and start sync testing.");
+            DoSomethingAsync();
+            Console.ReadKey();
+            Console.Clear();
+
+
             // Initialize wrapper with your dev key
             Wrapper wr = new Wrapper("your secret key");
 
-            string tag = Console.ReadLine();
+            string tag = "80RGVCV9C";
 
             Player player = wr.GetPlayer(tag);
 
@@ -24,7 +31,55 @@ namespace ExampleUsage
                 Console.WriteLine($"{c.name} {c.elixir} ({c.rarity}) --- level: {c.level}");
             }
 
+            Console.WriteLine("Searching for clan with name abc and at least 10 members.");
+
+            // Parameters: name, min score, min members, max members. When is some parameter null, wrapper will not search using the parameter. At least
+            // one parameter must not be null.
+            SimplifiedClan[] searchResult = wr.SearchForClans("abc", null, 10, null);
+
+            Console.WriteLine($"First clan is \"{searchResult[0]}\" with {wr.GetClan(searchResult[0].tag).memberCount} members.");
+
             Console.ReadKey();
+        }
+
+        
+        static async void DoSomethingAsync()
+        {
+            Wrapper wr = new Wrapper("2970f9ab909243f2a86c1bda54bdf9bce950718c3029447686924cf9985af91c");
+
+            string tag = "80RGVCV9C";
+            string ctag = "22Y802";
+
+            Console.WriteLine("I'll get one player profile, one clan profile, best players and best clans async!");
+            
+            Task<Player> a_player = wr.GetPlayerAsync(tag);
+            Task<Clan> a_clan = wr.GetClanAsync(ctag);
+            Task<SimplifiedPlayer[]> a_topPlayers = wr.GetTopPlayersAsync(); // wr.GetTopPlayers() and its async version return SimplifiedPlayer -> this is just like Player,
+                                                                             // but simplified with less properties. If you want to get complete overview, get the top player:
+                                                                             // Player topPlayer = wr.GetPlayer(wr.GetTopPlayers()[0].tag)
+            Task<SimplifiedClan[]> a_topClans = wr.GetTopClansAsync(); // Here is the same thing as with GetTopPlayers()
+
+
+            // Wait untill everything is prepared. You can for example write dots into console
+            while (!a_player.IsCompleted || !a_clan.IsCompleted || !a_topPlayers.IsCompleted || !a_topClans.IsCompleted)
+            {
+                Console.Write(".");
+                System.Threading.Thread.Sleep(50);
+            }
+
+
+
+            Console.WriteLine("Done!");
+
+            Player player = await a_player;
+            Clan clan = await a_clan;
+            SimplifiedPlayer[] topPlayers = await a_topPlayers;
+            SimplifiedClan[] topCLans = await a_topClans;
+
+            Console.WriteLine(player.name);
+            Console.WriteLine(clan.name);
+            Console.WriteLine(topPlayers[0].name);
+            Console.WriteLine(topCLans[0].name);
         }
     }
 }
