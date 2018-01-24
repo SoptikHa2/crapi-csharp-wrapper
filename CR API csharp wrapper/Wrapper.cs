4da035ab62900507a70c83b8a9a5c04916705829
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2018-01-24 19:58:13 +0100

    Added regions to top clans
    Uer can now get top clans from whole world, or some region.

--- CR API csharp wrapper/Wrapper.cs
+++ CR API csharp wrapper/Wrapper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -25,6 +26,15 @@ namespace CRAPI
             Constants,
             Version,
             Tournaments
+        }
+
+        /// <summary>
+        /// All locations that can be used in TopClans. Select "None" to ignore location. _EU is Europe, _NA is North America, _SA is South America, _AS is Asia, _AU is Australia, _AF is Africa, _INT is International.
+        /// Full list can be seen here: https://raw.githubusercontent.com/cr-api/cr-api-data/master/json/regions.json
+        /// </summary>
+        public enum Locations
+        {
+            None, _EU, _NA, _SA, _AS, _AU, _AF, _INT, AF, AX, AL, DZ, AS, AD, AO, AI, AQ, AG, AR, AM, AW, AC, AU, AT, AZ, BS, BH, BD, BB, BY, BE, BZ, BJ, BM, BT, BO, BA, BW, BV, BR, IO, VG, BN, BG, BF, BI, KH, CM, CA, IC, CV, BQ, KY, CF, EA, TD, CL, CN, CX, CC, CO, KM, CG, CD, CK, CR, CI, HR, CU, CW, CY, CZ, DK, DG, DJ, DM, DO, EC, EG, SV, GQ, ER, EE, ET, FK, FO, FJ, FI, FR, GF, PF, TF, GA, GM, GE, DE, GH, GI, GR, GL, GD, GP, GU, GT, GG, GN, GW, GY, HT, HM, HN, HK, HU, IS, IN, ID, IR, IQ, IE, IM, IL, IT, JM, JP, JE, JO, KZ, KE, KI, XK, KW, KG, LA, LV, LB, LS, LR, LY, LI, LT, LU, MO, MK, MG, MW, MY, MV, ML, MT, MH, MQ, MR, MU, YT, MX, FM, MD, MC, MN, ME, MS, MA, MZ, MM, NA, NR, NP, NL, NC, NZ, NI, NE, NG, NU, NF, KP, MP, NO, OM, PK, PW, PS, PA, PG, PY, PE, PH, PN, PL, PT, PR, QA, RE, RO, RU, RW, BL, SH, KN, LC, MF, PM, WS, SM, ST, SA, SN, RS, SC, SL, SG, SX, SK, SI, SB, SO, ZA, KR, SS, ES, LK, VC, SD, SR, SJ, SZ, SE, CH, SY, TW, TJ, TZ, TH, TL, TG, TK, TO, TT, TA, TN, TR, TM, TC, TV, UM, VI, UG, UA, AE, GB, US, UY, UZ, VU, VA, VE, VN, WF, EH, YE, ZM, ZW
         }
 
 
@@ -99,8 +109,9 @@ namespace CRAPI
         /// </summary>
         /// <param name="include">Optional parameter, may be null. Specifies fields to be included in response. Everything else is dropped. This parameter and/or [exclude] parameter must be NULL.</param>
         /// <param name="exclude">Optional parameter, may be null. Specifies fields to be dropped from response. Everything else is delivered. This parameter and/or [include] parameter must be NULL.</param>
+        /// <param name="region">Optional parameter, default value is Locations.None. Specifies region, only clans from selected region will be returned. Select Locations.None to return clans from whole world.</param>
         /// <returns></returns>
-        public SimplifiedClan[] GetTopClans(string[] include = null, string[] exclude = null)
+        public SimplifiedClan[] GetTopClans(string[] include = null, string[] exclude = null, Locations region = Locations.None)
         {
             string query = String.Empty;
             if (include != null && exclude != null)
@@ -109,7 +120,7 @@ namespace CRAPI
                 query += "?keys=" + String.Join(",", include);
             if (exclude != null)
                 query += "?exclude=" + String.Join(",", exclude);
-            string output = Get(Endpoints.Top, "clans" + query);
+            string output = Get(Endpoints.Top, region != Locations.None ? "clans/" + region.ToString() + query : "clans" + query);
             return Parse<SimplifiedClan[]>(output);
         }
 
@@ -252,8 +263,9 @@ namespace CRAPI
         /// </summary>
         /// <param name="include">Optional parameter, may be null. Specifies fields to be included in response. Everything else is dropped. This parameter and/or [exclude] parameter must be NULL.</param>
         /// <param name="exclude">Optional parameter, may be null. Specifies fields to be dropped from response. Everything else is delivered. This parameter and/or [include] parameter must be NULL.</param>
+        /// <param name="region">Optional parameter, default value is Locations.None. Specifies region, only clans from selected region will be returned. Select Locations.None to return clans from whole world.</param>
         /// <returns></returns>
-        public async Task<SimplifiedClan[]> GetTopClansAsync(string[] include = null, string[] exclude = null)
+        public async Task<SimplifiedClan[]> GetTopClansAsync(string[] include = null, string[] exclude = null, Locations region = Locations.None)
         {
             string query = String.Empty;
             if (include != null && exclude != null)
@@ -262,7 +274,7 @@ namespace CRAPI
                 query += "?keys=" + String.Join(",", include);
             if (exclude != null)
                 query += "?exclude=" + String.Join(",", exclude);
-            Task<string> output = GetAsync(Endpoints.Top, "clans" + query);
+            Task<string> output = GetAsync(Endpoints.Top, region == Locations.None ? "clans" + query : "clans/" + region.ToString() + query);
             return Parse<SimplifiedClan[]>(await output);
         }
 
